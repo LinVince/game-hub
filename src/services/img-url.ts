31e3f3f08e2775f50847468516b2cee8c4b789cf
LinVince <vincejim91126@gmail.com>
2023-07-25 11:41:05 +0800

    optimizing image by cropping it

---
+++ src/services/img-url.ts
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const getCropedImageUrl = (url:string) => {
+  const target = '/media'
+  const index = url.indexOf(target)
+  const targetUrl = url.slice(0, index) + '/crop/600/400' + url.slice(index)  
+
+    return  targetUrl;
+}
+
+export default getCropedImageUrl
