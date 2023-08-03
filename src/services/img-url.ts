61eedbf2a1d2c6c85c7ef1bfeaae3e6cfc551816
LinVince <vincejim91126@gmail.com>
2023-08-03 17:49:48 +0800

    Sorting games

--- src/services/img-url.ts
+++ src/services/img-url.ts
@@ -2,6 +2,9 @@ import React from 'react'
 
 
 const getCropedImageUrl = (url:string) => {
+
+  if (!url) return ;
+
   const target = 'media/'
   const index = url.indexOf(target) + target.length
   const targetUrl = url.slice(0, index) + 'crop/600/400/' + url.slice(index)  
