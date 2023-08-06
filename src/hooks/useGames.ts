65fec8f73bd74a8a5ab4bd053fea0e2ff989409f
LinVince <vincejim91126@gmail.com>
2023-08-06 23:20:07 +0800

    fix platform bug

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -18,6 +18,6 @@ export interface Game {
     rating:number;
   }
   
-const useGames = (gameQuery:GameQuery) => useData<Game>('/games', {params:{genres : gameQuery.genre?.id, platforms: gameQuery.platform?.id, ordering: gameQuery.sortOrder, search:gameQuery.searchText}}, [gameQuery])
+const useGames = (gameQuery:GameQuery) => useData<Game>('/games', {params:{genres : gameQuery.genre?.id, parent_platforms: gameQuery.platform?.id, ordering: gameQuery.sortOrder, search:gameQuery.searchText}}, [gameQuery])
 
 export default useGames;
